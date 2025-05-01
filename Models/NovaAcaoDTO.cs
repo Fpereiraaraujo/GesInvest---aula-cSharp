6bcaead01415a0ba051b87eec8e8a0e8de52cf5d
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-05-01 11:30:12 -0300

    feat:criando primeira rota, adicionando entityFramework

---
+++ Models/NovaAcaoDTO.cs
@@ -0,0 +1,11 @@
+namespace GestInvest.Models
+{
+    public class NovaAcaoDTO
+    {
+        public string Nome { get; set; }
+        public string Empresa { get; set; }
+        public DateTime DataCompra { get; set; }
+        public string Moeda { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
